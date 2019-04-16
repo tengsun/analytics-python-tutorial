81fc83e63ecb2156f426d8a410d5300514477f38
tengsun <tengsun@cn.ibm.com>
2019-04-16 22:09:18 +0800

    add package test

---
+++ mypkg/__init__.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print('load from main')
+else:
+    print('load from external')
---
+++ pkg.py
@@ -0,0 +1,5 @@
+import mypkg.comp1 as c1
+import mypkg.comp2 as c2
+
+c1.test()
+c2.test()
